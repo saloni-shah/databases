e55bcb243c10056b4e9a50124afcd40bd48b804c
Hack Reactor Student <student@hackreactor.com>
2017-06-12 20:11:23 -0700

    first commit

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,7 +2,11 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) { 
+      // console.log("saloni");
+      // res.writeHead(200);
+      // res.end();
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,24 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  userID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username CHAR(10)
+);
+
+CREATE TABLE rooms (
+  roomID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  roomname CHAR(10)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  messageID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  message CHAR(20),
+  roomID int,
+  userID int,
+  FOREIGN KEY (roomID) REFERENCES rooms(roomID),
+  FOREIGN KEY (userID) REFERENCES users(userID)
 );
 
 /* Create other tables and define schemas for them here! */
